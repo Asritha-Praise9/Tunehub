171e79b1697ae5230b009cfd60b9f620c7a1cfe4
naveen-263 <naninaveen263@gmail.com>
2025-03-14 14:30:02 +0530

    done

---
+++ src/main/java/com/example/demo/entities/LoginData.java
@@ -0,0 +1,36 @@
+package com.example.demo.entities;
+
+public class LoginData {
+    String email;
+    String password;
+	public LoginData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LoginData(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "LoginData [email=" + email + ", password=" + password + "]";
+	}
+	
+	
+    
+}
